8e0238f46164d247d8d01867b4107d5bfa67b180
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-08-19 18:21:53 +0100

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,10 @@
+import About from "@/components/about";
 import Hero from "@/components/home/hero";
 
 
 export default function Home() {
   return <main className="w-full flex-1">
     <Hero />
+    <About />
   </main>;
-}
+} 

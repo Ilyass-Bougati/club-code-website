82b95c2414f5f005e247205d95b5870e5deb8a97
Monassib <monassib.ma@gmail.com>
2025-08-20 12:51:42 +0200

    Save changes before switching branch

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,7 +6,8 @@ import Hero from "@/components/home/hero";
 export default function Home() {
   return <main className="w-full flex-1">
     <Hero />
-         <JoinUs />
-     <Footer />
+
+    <JoinUs />
+    <Footer />
   </main>;
 }

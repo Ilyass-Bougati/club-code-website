ef9e38f443017ecc77eb0c379aa27d4509970c40
Ilyass-Bougati <i.bougati12@gmail.com>
2025-08-21 13:24:39 +0100

    Made uploading image better

--- server/src/main/java/com/code/server/service/Image/ImageServiceImpl.java
+++ server/src/main/java/com/code/server/service/Image/ImageServiceImpl.java
@@ -6,8 +6,10 @@ import com.code.server.entity.Image;
 import com.code.server.exception.NotFoundException;
 import com.code.server.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
@@ -18,6 +20,7 @@ public class ImageServiceImpl implements ImageService {
 
     private final ImageRepository imageRepository;
     private final ImageMapper imageMapper;
+    private final UploadImageService uploadImageService;
 
     @Override
     public ImageDto save(ImageDto imageDto) {
@@ -56,5 +59,16 @@ public class ImageServiceImpl implements ImageService {
         return imageRepository.findById(id)
                 .map(imageMapper::toDTO)
                 .orElseThrow(() -> new RuntimeException("Image not found"));
+    }
+
+    @Override
+    public ImageDto uploadImage(MultipartFile file) {
+        String[] splitName = file.getOriginalFilename().split("\\.");
+        String suffix = splitName[splitName.length - 1];
+        if (suffix.equals("jpg") || suffix.equals("png")) {
+            return save(uploadImageService.uploadImage(file));
+        } else {
+            throw new RuntimeException("Unsupported image format");
+        }
     }
 }

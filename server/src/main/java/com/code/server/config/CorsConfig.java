7cb27f95cf7166fbbf06000f3c8cb738fa84f4da
Ilyass bougati <i.bougati12@gmail.com>
2025-09-04 13:53:25 +0100

    Cors

---
+++ server/src/main/java/com/code/server/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.code.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reactor.util.annotation.NonNullApi;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // apply to all endpoints
+                        .allowedOrigins("*") // allow all origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}

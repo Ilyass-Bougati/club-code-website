2c56acb7ab7a637205ec2beed447ef07a3f0a42d
Ilyass bougati <i.bougati12@gmail.com>
2025-09-06 20:07:42 +0100

    A better way for metrics

--- server/src/main/java/com/code/server/controller/AuthController.java
+++ server/src/main/java/com/code/server/controller/AuthController.java
@@ -7,16 +7,14 @@ import com.code.server.service.jwt.TokenService;
 import com.code.server.dto.member.MemberRegisterRequest;
 import com.code.server.service.member.MemberService;
 import com.code.server.service.member.security.CustomUserDetails;
-import com.code.server.service.metrics.MetricsService;
 import com.code.server.utils.CookieUtils;
 import com.code.server.utils.Registration;
+import io.micrometer.core.annotation.Timed;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +28,6 @@ public class AuthController {
 
     private final TokenService tokenService;
     private final MemberService memberService;
-    private final MetricsService metricsService;
 
     @GetMapping("/registration")
     public ResponseEntity<RegistrationOpenResponse> checkRegistration() {
@@ -38,9 +35,9 @@ public class AuthController {
         return ResponseEntity.ok(res);
     }
 
+    @Timed(value = "auth.login", description = "The time it takes for login")
     @PostMapping("/login")
     public ResponseEntity<?> token(@RequestBody @Valid LoginRequest loginRequest) {
-        metricsService.countLogin();
         Token token = tokenService.login(loginRequest);
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, CookieUtils.genCookie("access_token", token.getAccess_token(), 60 * 60, "/").toString())
@@ -48,9 +45,9 @@ public class AuthController {
                 .body(Map.of("message", "Logged in successfully"));
     }
 
+    @Timed(value = "auth.refresh", description = "The time it takes for refresh the token")
     @PostMapping("/refresh")
     public ResponseEntity<?> refreshToken(@CookieValue(name = "refresh_token", required = true) String refreshToken) {
-        metricsService.countRefresh();
         Token token = tokenService.refreshToken(refreshToken);
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, CookieUtils.genCookie("access_token", token.getAccess_token(), 60 * 60, "/").toString())
@@ -67,13 +64,13 @@ public class AuthController {
                 .body(Map.of("message", "Logout successfully"));
     }
 
+    @Timed(value = "auth.register", description = "The time it takes to register a new user")
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.OK)
     public void saveClient(Principal principal, @RequestBody @Valid MemberRegisterRequest registerRequest) {
         if (principal != null) {
             throw new RuntimeException("Already logged in");
         }
-        metricsService.countRegister();
         memberService.register(registerRequest);
     }
 

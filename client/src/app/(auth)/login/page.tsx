d4d670120454e093ac0595c86d8401520f4d789a
Ziane-Badreddine <zianebadredddine2004@gmail.com>
2025-09-09 13:56:15 +0100

    fix:  redirct login

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,31 +1,30 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import { LoginForm } from "@/components/auth/login-form";
 import { useUser } from "@/hooks/use-user";
 import { Loading } from "@/components/loading";
 
 export default function LoginPage() {
-  const { user, error, loading } = useUser();
-  const router = useRouter();
+  const { user, loading, error } = useUser();
+  if (user) {
+    redirect("/");
+  }
 
-  // Redirect if logged in
-  useEffect(() => {
-    if (!loading && user) {
-      router.replace("/"); // replace avoids back button loop
-    }
-  }, [loading, user, router]);
-
-  if (loading) {
+  if (loading)
     return (
       <div className="flex min-h-svh items-center justify-center">
         <Loading />
       </div>
     );
+
+  if (error) {
+    return (
+      <div className="flex min-h-svh items-center justify-center">
+        <p className="text-red-500">Something went wrong. Please try again.</p>
+      </div>
+    );
   }
-
-
 
   return (
     <div className="bg-background flex min-h-[80vh] flex-col items-center justify-center gap-6 p-6 md:p-10">
